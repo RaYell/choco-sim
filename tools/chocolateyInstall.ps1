779c4040ddbf4d30a74f60896e52e92657adc495
Lukasz Rajchel <lukasz@rajchel.pl>
2015-12-29 07:42:54 +0100

    Update chocolateyInstall.ps1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 ﻿$packageName = 'sim'
-$url = 'https://marketplace.sitecore.net/services/~/media/Marketplace/Modules/S/Sitecore_Instance_Manager/packages/SIM_14_rev_150618.ashx'
+$url = 'https://marketplace.sitecore.net/services/~/media/Marketplace/Modules/S/Sitecore_Instance_Manager/packages/SIM_14_rev_150921.ashx'
 
 Install-ChocolateyZipPackage $packageName $url "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
